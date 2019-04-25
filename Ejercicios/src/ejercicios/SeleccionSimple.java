fb526fec006c898205db0f32e9a77165241509e5
LeonardoAV7 <aguilarvalarezo7@gmail.com>
2019-04-25 11:30:31 -0500

    ejercicios 4

---
+++ Ejercicios/src/ejercicios/SeleccionSimple.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+import paquetedos.Operacion;
+/**
+ *
+ * @author Junior Aguilar
+ */
+public class SeleccionSimple {
+    public static void main(String[] args) {
+        // Metodo principal
+        // Importamos el scanner
+        Scanner entrada = new Scanner(System.in);
+        
+        // Declaramos las variables
+        String miMensaje = Operacion.mensaje;
+        int calificacion;
+        int calificacion_2;
+        
+        // Ingresamos por teclado la primera nota
+        System.out.println("Ingrese su nota: ");
+        calificacion = entrada.nextInt();
+        
+        if (calificacion >=85){
+            System.out.printf("%s %d\n",miMensaje, calificacion );
+        }
+        
+        // Ingresamos por teclado la segunda nota
+        System.out.println("Ingrese su nota: ");
+        calificacion_2 = entrada.nextInt();
+        
+        if (calificacion_2 >=85){
+            System.out.printf("%s %d\n", miMensaje, calificacion_2 );
+        }
+        
+        
+    } 
+    
+}
